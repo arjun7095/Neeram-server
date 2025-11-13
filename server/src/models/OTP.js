879627967e348c69a85d30a475299957292eaba2
arjun7095 <boyapatidurga8@gmail.com>
2025-11-14 02:17:27 +0530

    Update OTP expiration time from 1 minute to 2 minutes

--- server/src/models/OTP.js
+++ server/src/models/OTP.js
@@ -5,6 +5,6 @@ countryCode: { type: String, required: true },
   mobile: { type: String, required: true },
   role: { type: String, required: true },
   otp: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: '1m' },
+  createdAt: { type: Date, default: Date.now, expires: '2m' },
 });
 module.exports = mongoose.model('OTP', otpSchema);
